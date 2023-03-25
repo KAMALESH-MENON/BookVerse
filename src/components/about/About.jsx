7ad41e7a53b406d288b6bdbf26aa8a01d85da9e4
kamalesh <kamalcbe86@gmail.com>
2023-03-25 12:42:24 +0530

    nav bars v1

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,18 @@ import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container"> 
+        <div className="about_me">
+          <div className="about_me-img">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
